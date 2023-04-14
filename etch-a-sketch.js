5e5f2a6803bade89f8afd04c5556a682b17868fc
Matt Schwimmer <mattschwimmer@matts-mbp.lan>
2023-04-13 20:50:45 -0400

    Added Resize Button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,13 +4,17 @@ const container = document.querySelector('.container')
 function setGrid(numSquares) {
     // Removes all existing squares
     container.replaceChildren()
-    let sideLength = container.offsetWidth / numSquares
+    // sideLength is %
+    let sideLength = 100 / numSquares
+//    console.log("container width is " + container.offsetWidth)
+//    console.log("side length is " + sideLength)
     for (let i = 0; i < numSquares; i++){
         for (let j = 0; j < numSquares; j++){
             const div = document.createElement('div')
             div.classList.add('square')
-            div.style.width = sideLength
-            div.style.height = sideLength
+//            div.textContent = 'box'
+            div.style.width = sideLength + "%"
+            div.style.height = sideLength + "%"
             container.appendChild(div)
         }
     }
@@ -27,4 +31,19 @@ function setGrid(numSquares) {
 
 // First, create a grid of 16x16 divs
 setGrid(16)
-// TODO, add reset size functionality to button
+
+// Resize Button
+const resizeButton = document.querySelector("#reset-btn")
+resizeButton.addEventListener("click", () => {
+    console.log("Clicked resize button")
+    let newSize = prompt("How many squares per side?")
+    console.log(newSize)
+
+    if (newSize > 100){
+        alert("Too many squares, must be less than 100")
+        setGrid(16)
+    }
+    else {
+        setGrid(newSize)
+    }
+})
